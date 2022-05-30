6a7fe42d2013727aa422b25056fd458009102e98
carlos a sanchez <cuartitoup@gmail.com>
2022-05-30 17:03:32 -0300

    entrega1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
 let entradaNombre = prompt('Ingresar su nombre');
-let salida =  " Bienvenido" + " " + entradaNombre + " " + "a la pagina del supermercado El Tropezon";
-alert(salida);
+let entradaEdad = prompt('Ingresar su edad');
+if( entradaNombre == "" ){
+    alert("No ingreso su nombre");
+    let entradaNombre = prompt('Ingresar su nombre');
+    alert("Hola " + entradaNombre + " " +" a Supermercado El Tropezon online");
+}else{
+    alert("Hola " + entradaNombre + " " +" a Supermercado El Tropezon online");
+}
+
+
+for (let entradaEdad <1 ; entradaEdad > 25){
+alert("usted es joven " );}
+if for (let entradaEdad <26 ; entradaEdad > 40){
+    alert("le quedan pocos años de juventud " );}
+    if for (let entradaEdad <41 ; entradaEdad > 65){
+        alert("aproveche antes de jubilarse " );}
+        if for (let entradaEdad <66 ;){
+            alert("aproveche los descuentos de  jubilados " );}
+
+
+
+
+
+
 
 

1cc6daf71dcdf84ea69286fc8931c0e28dfebaf1
carlos a sanchez <cuartitoup@gmail.com>
2022-06-04 11:06:33 -0300

    entrega1

--- js/index.js
+++ js/index.js
@@ -1,27 +1,22 @@
 let entradaNombre = prompt('Ingresar su nombre');
 let entradaEdad = prompt('Ingresar su edad');
-if( entradaNombre == "" ){
+if (entradaNombre == "") {
     alert("No ingreso su nombre");
     let entradaNombre = prompt('Ingresar su nombre');
-    alert("Hola " + entradaNombre + " " +" a Supermercado El Tropezon online");
-}else{
-    alert("Hola " + entradaNombre + " " +" a Supermercado El Tropezon online");
+    alert("Hola " + entradaNombre + " " + " a Supermercado El Tropezon online");
+} else {
+    alert("Hola " + entradaNombre + " " + " a Supermercado El Tropezon online");
 }
 
 
-for (let entradaEdad <1 ; entradaEdad > 25){
-alert("usted es joven " );}
-if for (let entradaEdad <26 ; entradaEdad > 40){
-    alert("le quedan pocos años de juventud " );}
-    if for (let entradaEdad <41 ; entradaEdad > 65){
-        alert("aproveche antes de jubilarse " );}
-        if for (let entradaEdad <66 ;){
-            alert("aproveche los descuentos de  jubilados " );}
-
-
-
-
-
-
-
-
+for (let i = 0; i < 1; ++i) {
+    if (entradaEdad <= 1 && entradaEdad > 25) {
+        alert("usted es joven ");
+    } else if (entradaEdad <= 26 > 40 && entradaEdad > 40) {
+        alert("le quedan pocos años de juventud ");
+    } else if (entradaEdad <= 41 > 65 && entradaEdad > 65) {
+        alert("aproveche antes de jubilarse ");
+    } else if (entradaEdad <= 66) {
+        alert("aproveche los descuentos de  jubilados ");
+    }
+}

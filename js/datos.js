076d94379f75421f1053c3f63ba0b06fad92480b
carlos a sanchez <cuartitoup@gmail.com>
2022-08-10 13:48:27 -0300

    ultimaentrega

---
+++ js/datos.js
@@ -0,0 +1,71 @@
+const user = [{
+        "id": 1,
+        "nombre": "Azucar Arcor x 1 kg",
+        "precio": 110,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/azucar1kg.png"
+    },
+    {
+        "id": 2,
+        "nombre": "Cruz de Malta yerba x 500 gr ",
+        "precio": 250,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/cruzdemalta500gr.png"
+    },
+    {
+        "id": 3,
+        "nombre": "Fargo pan integral x 490 gr ",
+        "precio": 350,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/dobleintegralfargo500gr.png"
+    },
+    {
+        "id": 4,
+        "nombre": "higenok premiun x 50 metros ",
+        "precio": 450,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/higenolpremiun50m.png"
+    },
+    {
+        "id": 5,
+        "nombre": "la serenisima sachet variedad x 1 lt ",
+        "precio": 130,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/lecheserenisimasachet1lt (2).png"
+    },
+    {
+        "id": 6,
+        "nombre": "Mermelada bc variedad x 370 gr ",
+        "precio": 450,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/mermeladabc390gr.png"
+    },
+    {
+        "id": 7,
+        "nombre": "frigor Helados x 75 gr",
+        "precio": 250,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/novedadfrigorceliacos75gr.png"
+    },
+    {
+        "id": 8,
+        "nombre": "Polenta Prestopronta x 1 kg",
+        "precio": 110,
+        "destacado": true,
+        "imagen": "./fotosdeproductos/prestoprontaxkg.png"
+    },
+    {
+        "id": 9,
+        "nombre": "Terma variedad 1 lt",
+        "precio": 120,
+        "destacado": false,
+        "imagen": "./fotosdeproductos/termavariedad1lt.png"
+    },
+    {
+        "id": 10,
+        "nombre": "Valente budin x 230 gr",
+        "precio": 230,
+        "destacado": false,
+        "imagen": "./fotosdeproductos/valentebudin.png"
+    },
+]
